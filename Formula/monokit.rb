f157dc8f1ec7ed7ca20ae284c2da4041e1a927e5
Bram <brammyers@gmail.com>
2025-12-05 01:44:49 -0600

    Initial formula for monokit v0.1.1

---
+++ Formula/monokit.rb
@@ -0,0 +1,30 @@
+class Monokit < Formula
+  desc "Teletype-style scripting for a SuperCollider complex oscillator voice"
+  homepage "https://github.com/stolmine/monokit"
+  url "https://github.com/stolmine/monokit/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "6df9020b2fff3712c3a9bc31d8cd9b6357e486fe3f5d2e972c12820d1c50afee"
+  license "GPL-2.0"
+
+  depends_on "rust" => :build
+  depends_on "supercollider"
+
+  def install
+    system "cargo", "build", "--release", "--locked"
+    bin.install "target/release/monokit"
+    pkgshare.install "sc"
+  end
+
+  def caveats
+    <<~EOS
+      SuperCollider server files installed to:
+        #{pkgshare}/sc
+
+      User config stored at:
+        ~/.config/monokit/
+    EOS
+  end
+
+  test do
+    assert_match "monokit", shell_output("#{bin}/monokit --version 2>&1", 1)
+  end
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Tap for Monokit
+
+Teletype-style scripting for a SuperCollider complex oscillator voice.
+
+## Installation
+
+```bash
+brew tap stolmine/monokit
+brew install monokit
+```
+
+## Requirements
+
+- macOS
+- SuperCollider (installed automatically via Homebrew)
+
+## More Info
+
+- [Monokit Repository](https://github.com/stolmine/monokit)
